3cf3eed219f7ab71d91736c5893fa46b2fae8032
xz3phyr <xz3phyr@proton.me>
2024-12-04 10:05:12 +0100

    rebase y arreglado el mapa

--- main.py
+++ main.py
@@ -1,49 +1,161 @@
 import pyxel
 
-from pacman import Pacman
-from fantasma import Fantasma
-from laberinto import Laberinto
-from vector import Vector
+grid = [  # The maze : 0 = empty square, 1 = wall, 2 = dot, 3 = large dot
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1],
+        [1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1],
+        [1, 1, 1, 3, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 3, 1, 1, 1],
+        [1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1],
+        [1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1],
+        [1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1],
+        [1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1],
+        [1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 1, 2, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 2, 1, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 1, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 0, 0, 0, 0, 0, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 0, 0, 0, 0, 0, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1],
+        [1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1],
+        [1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1],
+        [1, 1, 1, 3, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 3, 1, 1, 1],
+        [1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1],
+        [1, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1, 1, 1],
+        [1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1],
+        [1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1],
+        [1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ]
 
-W_ANCHURA = 224
-W_ALTURA = 288
+direction_offset = {
+    0: (0, 1),  # Abajo
+    1: (0, -1),  # Arriba
+    2: (-1, 0),  # Izquierda
+    3: (1, 0)  # Derecha
+}
 
-P_SPEED = 1 # Velocidad del pacman
-F_SPEED = 0.75 # Velocidad de los fantasmas
 
-H_OFFSET = 3 # Distancia en tiles entre el techo y el laberinto
+class Pacman:
+    def __init__(self):
+        self.x = 104  # posición de inicio
+        self.y = 184
+        self.x_grid = 13
+        self.y_grid = 23
+        self.input_direction = 2
+        self.direction = 2
+        self.walking = True
+        self.mouth = 2
+        self.i_mouth = 0
+
+    def draw_pacman(self):
+        x = self.x - 2
+        y = self.y - 3
+
+        tile_x = {1: 19, 2: 3}
+        tile_y = {0: 49, 1: 33, 2: 17, 3: 1}
+
+        if self.mouth == 0:
+            pyxel.blt(x, y, 1, 35, 1, 13, 13, 0)
+        else:
+            pyxel.blt(x, y, 1, tile_x[self.mouth], tile_y[self.direction], 13, 13, 0)
+
+    def main(self):
+        self.pacman_direction()
+        self.pacman_mouth()
+
+        if self.x % 8 == 0 and self.y % 8 == 0:
+            self.x_grid = self.x // 8 + 2  # +2 = 2 columnas de offset
+            self.y_grid = self.y // 8
+            self.pacman_eats()
+            self.check_walls()
+
+        self.teleportation()
+        self.pacman_movement()
+
+    def pacman_direction(self):
+        direction_keys = {
+            0: (pyxel.KEY_DOWN, pyxel.KEY_S),
+            1: (pyxel.KEY_UP, pyxel.KEY_W),
+            2: (pyxel.KEY_LEFT, pyxel.KEY_A),
+            3: (pyxel.KEY_RIGHT, pyxel.KEY_D)
+        }
+        for i, (key1, key2) in direction_keys.items():
+            if pyxel.btn(key1) or pyxel.btn(key2):
+                self.input_direction = i
+                break
+
+    def pacman_mouth(self):
+        converter = {0: 0, 1: 0, 2: 1, 3: 1, 4: 2, 5: 2, 6: 1, 7: 1}
+
+        if self.walking:
+            self.mouth = converter[self.i_mouth]
+            self.i_mouth = (self.i_mouth + 1) % 8
+        elif self.mouth == 0:
+            self.i_mouth = 2
+            self.mouth = 1
+
+    def pacman_eats(self):
+        grid[self.y_grid][self.x_grid] = 0
+
+    def check_walls(self):
+        x_offset, y_offset = direction_offset[self.input_direction]
+        x_offset2, y_offset2 = direction_offset[self.direction]
+
+        if grid[self.y_grid + y_offset][self.x_grid + x_offset] != 1:
+            self.direction = self.input_direction
+            self.walking = True
+        elif grid[self.y_grid + y_offset2][self.x_grid + x_offset2] == 1:
+            self.walking = False
+
+    def teleportation(self):
+        x = {2: 224, 3: -8}
+        if self.x <= -8 or self.x >= 224:
+            self.x = x[self.direction]
+
+    def pacman_movement(self):
+        if self.walking:
+            offset_x, offset_y = direction_offset[self.direction]
+            self.x += offset_x
+            self.y += offset_y
+
 
 class App:
     def __init__(self):
-        self.resolution = Vector(W_ANCHURA, W_ALTURA) * 2
-        pyxel.init(self.resolution.x, self.resolution.y, title="PAC-MAN", fps=60, quit_key=pyxel.KEY_Q, display_scale=1)
-        pyxel.load("assets.pyxres")
-        self.started = False
+        pyxel.init(224, 248, "Pac-Man", 65, pyxel.KEY_ESCAPE, 4)
+        pyxel.load("theme.pyxres")
+        self.pacman = Pacman()
         pyxel.run(self.update, self.draw)
 
-    def init_game(self):
-        # Inicializamos clases "Singleton" (mirar singleton.py)
-        self.laberinto = Laberinto(self.resolution, H_OFFSET)
-        pacman_spawn = Vector((self.resolution.x / 2), (self.resolution.y / 2) + (self.laberinto.tile_size * (H_OFFSET - 1/2)))
-        fantasmas_spawn = self.resolution // 2
-        self.pacman = Pacman(pacman_spawn, P_SPEED)
-        self.fantasma = Fantasma(fantasmas_spawn, F_SPEED)
+    # Devuelve False si no hay puntos en el tablero
+    def draw_dots(self):
+        dot_exists = False
+        for column_number in range(2, len(grid[0])):  # 2 = dos columnas de offset
+            for line_number in range(len(grid)):
+                if grid[line_number][column_number] == 2:
+                    pyxel.rect(column_number * 8 + 3 - 16, line_number * 8 + 3, 2, 2, 2)  # -16 = dos columnas de offset
+                    dot_exists = True
+                elif grid[line_number][column_number] == 3:
+                    pyxel.blt(column_number * 8 - 16, line_number * 8, 0, 224, 0, 8, 8, 0)  # -16 = dos columnas de offset
+                    dot_exists = True
+        return dot_exists
 
     def update(self):
-        if (not self.started):
-            # if (pyxel.btnp(pyxel.KEY_RETURN)):
-                self.init_game()
-                self.started = True
+        self.pacman.main()
 
-        if (self.started):
-            self.pacman.update()
-            self.fantasma.update()
-        
     def draw(self):
         pyxel.cls(0)
-        if (self.started):
-            self.laberinto.draw()
-            self.pacman.draw()
-            self.fantasma.draw()
+        if (self.draw_dots()):
+            pyxel.blt(0, 0, 0, 0, 0, 224, 248, 0)
+            self.pacman.draw_pacman()
+        else:
+            pyxel.text(80, 120, "YOU WIN", 7)
+
 
 App()
--- singleton.py
+++
@@ -1,9 +0,0 @@
-# para tener variables de clases "globales" ya que no se pueden usar variables globales en python.
-# https://stackoverflow.com/questions/6760685/what-is-the-best-way-of-implementing-singleton-in-python
-
-class Singleton(object):
-    _instance = None
-    def __new__(cls, *args, **kwargs):
-        if not isinstance(cls._instance, cls):
-            cls._instance = super(Singleton, cls).__new__(cls)
-        return cls._instance
