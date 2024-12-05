6293ee6d8452b8d8ba71ec884130f23340458095
xz3phyr <xz3phyr@proton.me>
2024-12-05 11:00:01 +0100

    frutas y pantalla victoriosa

---
+++ frutas.py
@@ -0,0 +1,74 @@
+import random
+import pyxel
+
+class Frutas:
+    def __init__(self, laberinto):
+        self.laberinto = laberinto
+        self._posicion_frutas = [] # Posiciones de las frutas
+        self.fruta1_spawned = False
+        self.fruta2_spawned = False
+
+    @property
+    def posicion_frutas(self):
+        return self._posicion_frutas
+
+    def spawn_fruta(self, is_fruta1): # Añade una fruta al tablero
+
+        if is_fruta1 and not self.fruta1_spawned:
+            self.fruta1_spawned = True
+        elif not is_fruta1 and not self.fruta2_spawned:
+            self.fruta2_spawned = True
+        else:
+            return # No spawnear más frutas
+
+        # Encuentra las celdas vacías
+        empty_spaces = [(y, x) for y in range(len(self.laberinto.grid)) for x in range(len(self.laberinto.grid[0])) if self.laberinto.grid[y][x] == 0]
+        
+        if empty_spaces:
+            y, x = 0, 0
+
+            # Evitamos los bordes del mapa y la prisión del centro.
+            while ((y < 1 or y > 29) or (x < 2 or x > 29) or (y < 17 and y > 11 and x > 11 and x < 20)):
+                y, x = random.choice(empty_spaces)
+            
+            self._posicion_frutas.append((y, x))
+            
+            fruit = random.randint(5, 8) # Las frutas van del 5 al 8
+            self.laberinto.grid[y][x] = fruit  # Añade la fruta al tablero
+
+    def draw_fruits(self):
+        # Dibuja las frutas dependiendo de su tipo
+        for y, x in self._posicion_frutas:
+            u_coord = 0
+            if self.laberinto.grid[y][x] == 5:
+                u_coord = 32
+            elif self.laberinto.grid[y][x] == 6:
+                u_coord = 48
+            elif self.laberinto.grid[y][x] == 7:
+                u_coord = 64
+            elif self.laberinto.grid[y][x] == 8:
+                u_coord = 80
+            pyxel.blt(x * 8 - 22, y * 8 - 4, 0, u_coord, 48, 16, 16, scale=1, colkey=0)
+
+    def check_colision(self, pacman): # Revisamos colisión con Pac-Man
+        if (pacman.y_grid, pacman.x_grid) in self._posicion_frutas:
+            self._posicion_frutas.remove((pacman.y_grid, pacman.x_grid))
+
+            # Aumentamos el puntaje
+            if self.laberinto.grid[pacman.y_grid][pacman.x_grid] == 5:
+                pacman.score += 250
+            elif self.laberinto.grid[pacman.y_grid][pacman.x_grid] == 6:
+                pacman.score += 300
+            elif self.laberinto.grid[pacman.y_grid][pacman.x_grid] == 7:
+                pacman.score += 350
+            elif self.laberinto.grid[pacman.y_grid][pacman.x_grid] == 8:
+                pacman.score += 400
+
+            self.laberinto.grid[pacman.y_grid][pacman.x_grid] = 0
+            return True
+        return False
+
+    def reset_frutas(self):
+        self._posicion_frutas = []
+        self.fruta1_spawned = False
+        self.fruta2_spawned = False
--- main.py
+++ main.py
@@ -1,47 +1,69 @@
 import pyxel
 from laberinto import Laberinto
 from pacman import Pacman
-from laberinto import normal_direction
+from frutas import Frutas
+import time
 
 class App:
     def __init__(self):
-        pyxel.init(224, 280, "Pac-Man", 65, pyxel.KEY_ESCAPE, 4)
+        pyxel.init(224, 260, "Pac-Man", 65, pyxel.KEY_ESCAPE, 4)
         pyxel.load("assets/theme.pyxres")
         self.laberinto = Laberinto()
         self.pacman = Pacman(self.laberinto)
+        self.frutas = Frutas(self.laberinto)
+        self.minutos_finales, self.segundos_finales = -1, -1
         pyxel.run(self.update, self.draw)
 
     # Devuelve False si no hay puntos en el tablero
     def draw_dots(self):
-        dot_exists = False
+        dot_number = 0
         for column_number in range(2, len(self.laberinto.grid[0])):  # 2 = dos columnas de offset
             for line_number in range(len(self.laberinto.grid)):
                 if self.laberinto.grid[line_number][column_number] == 2:
                     pyxel.rect(column_number * 8 + 3 - 16, line_number * 8 + 3, 2, 2, 15)  # -16 = dos columnas de offset
-                    dot_exists = True
+                    dot_number += 1
                 elif self.laberinto.grid[line_number][column_number] == 3:
                     pyxel.blt(column_number * 8 - 16, line_number * 8, 1, 224, 0, 8, 8)  # -16 = dos columnas de offset
-                    dot_exists = True
-        return dot_exists
+                    dot_number += 1
+        return dot_number
 
     def update(self):
         self.pacman.update()
+        self.frutas.check_colision(self.pacman)
 
     def draw(self):
         pyxel.cls(0)
+
+        dots_left = self.draw_dots()
+
         if (self.pacman.lives == 0): # Si pacman no tiene vidas
             pyxel.text(100, 140, "GAME OVER", 7)
-        elif (self.draw_dots()): # Si hay puntos en el tablero
+        elif dots_left: # Si hay puntos en el tablero
             pyxel.blt(0, 0, self.laberinto.level_image(), 0, 0, 224, 248, 0) # Dibujamos el mapa
+            if ((dots_left == 70 or dots_left == 170) and (len(self.frutas.posicion_frutas) == 0)): # Si se ha comido una cantidad de puntos
+                self.frutas.spawn_fruta(dots_left == 70)
             self.pacman.draw_pacman()
-            pyxel.text(2, 250, "Puntaje: " + str(self.pacman.score), 7)
-            pyxel.text(2, 260, "Vidas: " + str(self.pacman.lives), 7)
-            pyxel.text(2, 270, "Nivel: " + str(self.laberinto.level), 7)
+            self.frutas.draw_fruits()
+            pyxel.text(2, 251, "Vidas: " + str(self.pacman.lives), 7)
+            pyxel.text(40, 251, "Nivel: " + str(self.laberinto.level), 7)
+            pyxel.text(80, 251, "Puntaje: " + str(self.pacman.score), 7)
         elif (self.laberinto.level < 3): # Si no es el último nivel
+            time.sleep(2) # Esperamos 2 segundos para que la transición sea más suave
             self.laberinto.level += 1 # Pasamos al siguiente nivel
             self.pacman.set_pacman() # Reseteamos a Pac-Man
+            self.frutas.reset_frutas() # Reseteamos las frutas
         else: # Si llega al último nivel
-            pyxel.text(100, 140, "HAS GANADO !!!", 7)
+            
+            # String para el tiempo
+            if (self.minutos_finales == -1 or self.segundos_finales == -1):
+                tiempo_total = pyxel.frame_count / 60
+                self.minutos_finales = int(tiempo_total // 60)
+                self.segundos_finales = int(tiempo_total % 60)
+            
+            pyxel.text(100, 50, "HAS GANADO !!!", 7)
+            pyxel.text(100, 60, "Vidas: " + str(self.pacman.lives), 7)
+            pyxel.text(100, 70, "Tiempo: " + str(self.minutos_finales) + ":" + str(self.segundos_finales), 7)
+            pyxel.blt(75, 150, 0, 32, 16, 32, 32, scale=4, rotate=45, colkey=0) # El Pac-Man del final
 
 
 App()
